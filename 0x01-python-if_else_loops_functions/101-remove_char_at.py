8ee428d7ebf1a9f9536ee0e874f667f04501e7d1
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-08-24 01:12:31 +0000

    [update]corrected indentation

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,10 +2,10 @@
 def remove_char_at(str, n):
     s = ""
     for ch in str:
-       if n >= 0 and n < len(str):
-           if (ch == str[n]):
-               continue
-           s += ch
-       else:
-           s += ch
+        if n >= 0 and n < len(str):
+            if (ch == str[n]):
+                continue
+            s += ch
+        else:
+            s += ch
     return s
