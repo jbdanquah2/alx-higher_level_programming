3b45740cea29c1217bb87edeec4a8b0d65a5909a
John Danquah-Boateng <John Danquah-Boateng>
2022-08-26 18:40:13 +0000

    [feature]prints all names defined by compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    lst = dir(hidden_4)
+    for i in range(1, len(lst)):
+        if lst[i][0] != "_":
+            print("{:s}".format(lst[i]))
