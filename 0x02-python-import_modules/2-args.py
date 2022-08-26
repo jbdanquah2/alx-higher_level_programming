7000af702481ffd990e682420b92ad6c486c5f3f
John Danquah-Boateng <John Danquah-Boateng>
2022-08-26 12:30:49 +0000

    [update]fix bugs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,9 @@
 from sys import argv
 if __name__ == "__main__":
     length = len(argv)
-    arg = "arguments:" if length - 1 > 1 else "argument."
-    print("{:d} {:s}".format(length - 1, arg))
+    arg = "arguments" if (length - 1 > 1) or (length - 1 == 0) else "argument"
+    punc = ":" if (length - 1) >= 1 else "."
+    print("{:d} {:s}{:s}".format((length - 1), arg, punc))
     if length > 1:
         for i in range(1, length):
             print("{:d}: {:s}".format(i, argv[i]))
