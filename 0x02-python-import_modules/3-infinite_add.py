c1838580950c6e5e77c274cf8a9b9343aabd292e
John Danquah-Boateng <John Danquah-Boateng>
2022-08-26 14:23:17 +0000

    [update]fix bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-length = len(sys.argv)
-sum = 0
-if length > 1:
-    for i in range(1, length):
-        sum += int(sys.argv[i])
-print("{}".format(sum))
+from sys import argv
+if __name__ == "__main__":
+    length = len(argv)
+    sum = 0
+    if length > 1:
+        for i in range(1, length):
+            sum += int(argv[i])
+    print("{:d}".format(sum))
