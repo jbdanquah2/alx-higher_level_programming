c9944846700d0e409494488b4331d4353734d650
John Danquah-Boateng <John Danquah-Boateng>
2022-08-27 22:47:57 +0000

    [feature ]remove cC from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for str in my_string:
+        if str in "cC":
+            continue
+        new_string += str
+    return new_string
