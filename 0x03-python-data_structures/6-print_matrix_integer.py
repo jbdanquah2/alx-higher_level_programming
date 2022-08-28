8b203410016dd3e938644fe3930740cf02d4b993
John Danquah-Boateng <John Danquah-Boateng>
2022-08-28 08:46:23 +0000

    [update]fix bugs

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         s = " "
         for j in range(len(i)):
-            if j == len(i) -  1:
+            if j == len(i) - 1:
                 s = ""
             print("{:d}".format(j), end=s)
         print("\r")
