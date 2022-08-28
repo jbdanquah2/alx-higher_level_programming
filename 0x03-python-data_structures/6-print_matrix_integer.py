acc8f7734f7070dcf968842e22b244bbcee6de26
John Danquah-Boateng <John Danquah-Boateng>
2022-08-28 08:42:24 +0000

    [update]fix bugs

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[]):
+    for i in matrix:
+        s = " "
+        for j in range(len(i)):
+            if j == len(i) -  1:
+                s = ""
+            print("{:d}".format(j), end=s)
+        print("\r")
