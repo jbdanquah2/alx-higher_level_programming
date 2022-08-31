c55cf996b025b0a87a15c2ac0f2ff442635c7d40
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-08-31 22:44:42 +0000

    [feature]adds unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if isinstance(my_list):
+    if isinstance(my_list, list):
         sum = 0
         new_list = list(set(my_list))
         for el in new_list:
