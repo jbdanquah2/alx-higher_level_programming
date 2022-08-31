e24ef78afa565828edca57d558f780cfabee5cbf
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-08-31 23:19:43 +0000

    [update]bug fixes

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = []
-    for el in set_1:
-        for ell in set_2:
-            if el == ell:
-                break
-            new_list.append(el)
-            new_list.append(ell)
-    return new_list
+    s1 = list(set_1 - set_2)
+    s2 = list(set_2 - set_1)
+    return s1 + s2
