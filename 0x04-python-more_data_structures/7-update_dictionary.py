5f08e1eaedc2a0a35c7d696082a320c6b474c445
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-01 05:46:36 +0000

    [update]resolve bugs

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    new_dict = {}
     if isinstance(a_dictionary, dict):
-        a_dictionary[key] = value
+        for k, v in a_dictionary.items():
+            if k == key:
+                v = value
+            new_dict[k] = v
+    return new_dict
