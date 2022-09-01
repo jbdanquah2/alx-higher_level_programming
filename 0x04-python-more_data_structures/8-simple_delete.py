e2b1a68dd1bb257d959d4171229664e44ca6e59c
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-01 22:32:35 +0000

    [update]fix bugs

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if isinstance(a_dictionary, dict):
-        new_dict = {}
-        for k, v in a_dictionary.items():
-            new_dict[k] = v
-        del new_dict[k]
-    return new_dict
+        if key in a_dictionary:
+            del a_dictionary[key]
+    return a_dictionary
