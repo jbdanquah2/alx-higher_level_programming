0761ebb2a8bf1f2d0968d8ffb43fc5372647c29d
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-14 00:31:01 +0000

    bugs fixes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """A class for Square type class"""
+    def __init__(self, size):
+        """Init for the square class
+        Args:
+        param1: size is a private attribute of type int
+        """
+        self.__size = size
