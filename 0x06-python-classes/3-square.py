7afbf4918bb31568c2d78c9f88189dec9d24902e
John Danquah-Boateng <John Danquah-Boateng>
2022-09-14 13:30:15 +0000

    add task two

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a module for Square class"""
+
+
+class Square:
+    """A class for Square type class"""
+
+    def __init__(self, size=0):
+        """Init for the square class
+        Args:
+        param1: size is a private attribute of type int
+        """
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a square"""
+        return (self.__size ** 2)
