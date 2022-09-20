442778bb64d0ce83b5ade109430d77f533c5d2ca
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-20 18:19:38 +0000

    remove white spaces

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module implements a LockedClass
 """
+
+
 class LockedClass:
     """This class only allows first_name attribute to be created
-    """   
+    """
+
     __slots__ = ["first_name"]
