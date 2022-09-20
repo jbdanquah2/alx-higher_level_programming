99c50251bfebeebf3ad01fc9286683c180a53acd
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-20 18:17:56 +0000

    implements a lockedclass

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module implements a LockedClass
+"""
+class LockedClass:
+    """This class only allows first_name attribute to be created
+    """   
+    __slots__ = ["first_name"]
