ba4b2c899287a5343d03edb53d3e491e72fdacc4
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-21 07:08:48 +0000

    remove whitespaces in 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,13 +18,13 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if (not isinstance(matrix, list) or matrix == [] or
         not all(isinstance(lst, list) for lst in matrix) or
         not all((isinstance(ele, int) or isinstance(ele, float))
-            for ele in [number for row in matrix for number in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                for ele in [number for row in matrix for number in row])):
+        raise TypeError("matrix must be a matrix(list of lists)
+                        of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
