4e85f1f8d88842a1ec55bf1269e92cd8054f4367
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-21 07:43:57 +0000

    fix long string issue

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,8 +23,8 @@ def matrix_divided(matrix, div):
         not all(isinstance(lst, list) for lst in matrix) or
         not all((isinstance(ele, int) or isinstance(ele, float))
                 for ele in [number for row in matrix for number in row])):
-        raise TypeError("""matrix must be a matrix(list of lists)
-                        of integers/floats""")
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
