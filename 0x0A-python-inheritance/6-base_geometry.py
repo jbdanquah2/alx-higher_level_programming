693d28d3d37f0c3bcef49112fee855e6a1f94317
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-26 22:20:52 +0000

    add funct area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""an empty class
+"""
+
+
+class BaseGeometry():
+    """this is just an empty class
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
