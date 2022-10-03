d973a214bcdb14ca6ce74be6c9a438982edcf120
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-10-03 23:34:21 +0000

    adds the square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the Square class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This is the Square class"""
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """This is the init function"""
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str function"""
+
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
