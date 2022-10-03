dc8fa59204f919af898eb241bad4b3d0faecb306
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-10-03 17:16:05 +0000

    includes a function to draw the rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,11 @@ class Rectangle(Base):
         """Computes the area of the rectangle"""
 
         return (self.__width * self.__height)
+
+    def display(self):
+        """Draws the rectangle with #"""
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
