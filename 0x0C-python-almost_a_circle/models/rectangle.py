1c204c24198488d6c1b05deaf951dce004042d96
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-10-03 16:57:42 +0000

    include checks to the values

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,11 @@ class Rectangle(Base):
     def width(self, width):
         """Width setter function"""
 
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = width
 
     @property
@@ -37,6 +42,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """Height setter function"""
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
 
         self.__height = height
 
@@ -50,6 +60,11 @@ class Rectangle(Base):
     def x(self, x):
         """x setter function"""
 
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = x
 
     @property
@@ -61,5 +76,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y setter function"""
+
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
 
         self.__y = y
