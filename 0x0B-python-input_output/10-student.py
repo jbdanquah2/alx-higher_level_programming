18d92a5d2bc13ae6f97f1510f38bfd5d4bc72e4a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-10-07 18:02:00 +0000

    fix bugs

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,6 +15,7 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieves a dict representation of Student"""
 
-        if attrs is None:
-        
+        if (isinstance(attrs, list) and all(type(elem) == str
+                                            for elem in attrs)):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
