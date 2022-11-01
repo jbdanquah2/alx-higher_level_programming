4186e0eaad1fcf5fcfa7adc8e3b473f4133bc5ff
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-01 21:21:23 +0000

    change character encoding

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- converts database and fileds to UTF8
+use hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0
+	CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE first_table
+	CONVERT TO CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
