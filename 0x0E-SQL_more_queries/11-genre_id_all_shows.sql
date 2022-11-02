6ab4488bfeb886b804a01ad73e54a60cc91fba62
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-02 21:25:44 +0000

    list all shows with genre id or null

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- list all tv shows and gener id
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
-    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
