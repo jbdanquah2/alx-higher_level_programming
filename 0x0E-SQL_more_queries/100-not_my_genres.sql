b7235cb9c485d1e2b5404dc12ab189cc7065d9c7
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-03 23:28:00 +0000

    add script to check specific genres

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,16 @@
 -- list all genres not linked to Dexter
-SELECT
-	DISTINCT tv_genres.name
-FROM tv_genres
-JOIN tv_show_genres
-	on tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title <> "Dexter"
-ORDER BY tv_genres.name ASC;
+SELECT DISTINCT 
+	tv_genres.name
+  FROM tv_genres 
+  	JOIN tv_show_genres 
+  		ON tv_genres.id = tv_show_genres.genre_id
+  	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+  WHERE tv_genres.name 
+  	NOT IN (SELECT DISTINCT
+  			tv_genres.name
+  		FROM tv_genres JOIN tv_show_genres
+  			ON tv_genres.id = tv_show_genres.genre_id
+  		JOIN tv_shows 
+  			ON tv_shows.id = tv_show_genres.show_id
+  		WHERE tv_shows.title = 'Dexter')
+  	ORDER BY tv_genres.name;
