e186e75e942d3c70328c738b50105a1ba6d3aa95
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-03 23:59:40 +0000

    fix bugs

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,12 @@
 SELECT DISTINCT 
 	ts.title
 	FROM tv_shows ts
-	JOIN tv_show_genres sh
-		ON ts.id = sh.show_id JOIN tv_genres tg
+	LEFT JOIN tv_show_genres sh
+		ON ts.id = sh.show_id 
+	LEFT JOIN tv_genres tg
 		ON tg.id = sh.genre_id
 	WHERE ts.title 
-	NOT IN (SELECT
+	NOT IN (SELECT DISTINCT
 			ts.title
 		FROM tv_shows ts
 		JOIN tv_show_genres sh
