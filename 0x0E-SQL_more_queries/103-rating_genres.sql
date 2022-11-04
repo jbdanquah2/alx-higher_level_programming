199f67a1de786719d9a821e870ab1b58a331bcb5
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-04 00:26:25 +0000

    add script to list all genres by rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,13 @@
+-- list all genres by rating
+SELECT
+	tg.name,
+	SUM(sr.rate) as ratings
+	FROM tv_genres tg
+	JOIN tv_show_genres sg
+		ON tg.id = sg.genre_id
+	JOIN tv_shows ts
+		ON ts.id = sg.show_id
+	JOIN tv_show_ratings sr
+		ON ts.id = sr.show_id
+	GROUP BY tg.name
+ORDER BY ratings DESC;
