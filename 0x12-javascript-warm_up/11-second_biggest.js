22d456bbf6ae683213ba3233575e716f3830eade
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-29 23:24:55 +0000

    searches the seconde biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 let biggest;
 let secondBiggest = 0;
-if (process.argv.length > 2 ) {
-    biggest = parseInt(process.argv[2]);
+if (process.argv.length > 2) {
+  biggest = parseInt(process.argv[2]);
 }
 
 for (let i = 2; i < process.argv.length; i++) {
-    if (biggest < parseInt(process.argv[i])) {
-        biggest = parseInt(process.argv[i]);
-    }
+  if (biggest < parseInt(process.argv[i])) {
+    biggest = parseInt(process.argv[i]);
+  }
 }
 for (let j = 2; j < process.argv.length; j++) {
-    if (secondBiggest < parseInt(process.argv[j]) && parseInt(process.argv[j]) !== biggest) {
-        secondBiggest = parseInt(process.argv[j]);
-    }
+  if (secondBiggest < parseInt(process.argv[j]) && parseInt(process.argv[j]) !== biggest) {
+    secondBiggest = parseInt(process.argv[j]);
+  }
 }
 console.log(secondBiggest);
