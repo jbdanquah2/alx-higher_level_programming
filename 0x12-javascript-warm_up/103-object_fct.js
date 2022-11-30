347849269ba6b4d78a9a64f04dd06a6259adf8f6
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-30 00:10:49 +0000

    inlcudes a function to increment object value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,10 +5,9 @@ const myObject = {
 };
 console.log(myObject);
 
-function incr () {
-  myObject.value += 1;
+myObject.incr = function () {
+  this.value += 1;
 }
-myObject.incr = incr;
 
 myObject.incr();
 console.log(myObject);
