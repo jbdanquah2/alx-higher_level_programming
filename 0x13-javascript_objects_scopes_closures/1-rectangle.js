bd194ee1b892f67ed733c5071bc0f93233787fcd
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-30 00:42:13 +0000

    adds constructor to Rectangle class

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,10 @@
-module.exports = class Rectangle {
+const Rectangle = require("./0-rectangle");
+
+class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 };
+
+module.exports = Rectangle;
