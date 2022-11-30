68b0f92c63a0305971f36d5a2f8af4fe6fa2e901
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-11-30 20:07:49 +0000

    extends a class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,14 +5,14 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint(c=undefined) {
-      for (let i = 0; i < this.height; i++) {
-        if (c !== undefined){
-          console.log(`${c}`.repeat(this.width));
-        } else {
-          console.log(`x`.repeat(this.width));
-        }      
+  charPrint (c = undefined) {
+    for (let i = 0; i < this.height; i++) {
+      if (c !== undefined) {
+        console.log(`${c}`.repeat(this.width));
+      } else {
+        console.log('x'.repeat(this.width));
       }
     }
+  }
 }
 module.exports = Square;
