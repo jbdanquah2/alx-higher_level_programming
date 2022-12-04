a2b50b8701cbfe6c59048d1acfa52536f5c8c17a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-12-04 19:10:31 +0000

    takes care of SQL injection

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name FROM cities c
+    cur.execute("SELECT c.id, c.name, s.name FROM cities c \
                 JOIN states s ON c.state_id=s.id ORDER BY id")
     [print(city) for city in cur.fetchall()]
