69ef7c2c7cf12d676fa0f9daac44af52c4f2a7e8
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2023-01-14 23:13:22 +0000

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map((ele, idx) => ele * idx);
-console.log(list);
-console.log(newList);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const f1 = fs.readFileSync(process.argv[2]);
-const f2 = fs.readFileSync(process.argv[3]);
-fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');
